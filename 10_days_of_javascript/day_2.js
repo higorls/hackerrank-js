10fbaacfdf551da768e9b5d021d30e237fb9cc8b
higorluizds <higorluizds@gmail.com>
2022-08-04 19:04:01 -0300

    [10 days of javascript] day 2 challenge

---
+++ 10_days_of_javascript/day_2.js
@@ -0,0 +1,8 @@
+function vowelsAndConsonants(s) {
+    for(let i=0;i<s.length;i++){
+        if(/^[aeiou]$/i.test(s[i])) console.log(s[i])
+    }
+    for(let i=0;i<s.length;i++){
+        if(!/^[aeiou]$/i.test(s[i])) console.log(s[i])
+    }
+}
